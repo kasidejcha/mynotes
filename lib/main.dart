11d7c342f52b2a1d4af63bb8c41eb7a14fa703b4
Kasidej Cha-umpong <kasidejcha.bas@gmail.com>
2023-03-29 23:27:13 +0700

    first_commit

---
+++ lib/main.dart
@@ -0,0 +1,57 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+import 'package:mynotes/views/login_view.dart';
+import 'package:mynotes/views/register_view.dart';
+
+import 'firebase_options.dart';
+
+void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: const HomePage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView(),
+      },
+    )
+    );
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+@override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+        future: Firebase.initializeApp(
+                  options: DefaultFirebaseOptions.currentPlatform
+                ),
+        builder: (context, snapshot) {
+          switch (snapshot.connectionState) {
+            case ConnectionState.done:
+              // final user = FirebaseAuth.instance.currentUser;
+              // final emailVerified = user?.emailVerified ?? false;
+              // if (emailVerified){
+              //   return const Text('Done');          
+              // } else {
+              //   return const VerifyEmailView();
+              // }
+              return const LoginView();
+            default:
+              return const CircularProgressIndicator();
+          }
+        },
+      );
+  }
+}
+
+
+
+
+
